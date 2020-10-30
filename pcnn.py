03836685cbd05ffc4064a04dc7456d8bedc7d0aa
Arnold Christian Veloso Lima <arnoldcvl@gmail.com>
2020-10-30 02:33:41 -0300

    Problems with convolution fixed. Stable version

--- pcnn.py
+++ pcnn.py
@@ -1,12 +1,16 @@
+import sys
 import numpy as np
 import cv2 as cv
+from scipy import signal
 
 Alpha_F = 0.1
 Alpha_L = 1.0
-Alpha_T = 1.0
+Alpha_T = 0.3
+
 V_F = 0.5
 V_L = 0.2
 V_T = 20.0
+
 Num = 10
 Beta = 0.1
 
@@ -14,8 +18,14 @@ W = [0.5, 1, 0.5, 1, 0, 1, 0.5, 1, 0.5,]
 W = np.array(W, np.float).reshape((3, 3))
 M = W
 
+global F
+global L
+global Y
+global T
+global Y_AC
+
 def main(argv):
-    default_file = 'sudoku.png'
+    default_file = 'test.png'
     filename = argv[0] if len(argv) > 0 else default_file
     
     # Loads an image
@@ -25,25 +35,36 @@ def main(argv):
     if src is None:
         print ('Error opening image!')
         print ('Usage: hough_lines.py [image_name -- default ' + default_file + '] \n')
-    return -1
+        return -1
+
+    cv.imshow("Original", src)
 
     dim = src.shape
 
-    global F = np.zeros( dim, np.float)
-    global L = np.zeros( dim, np.float)
-    global Y = np.zeros( dim, np.float)
-    global T = np.ones( dim, np.float)
-
+    F = np.zeros( dim, np.float)
+    L = np.zeros( dim, np.float)
+    Y = np.zeros( dim, np.float)
+    T = np.ones( dim, np.float)
+    Y_AC = np.zeros( dim, np.float)
+    
     #normalize image
     S = cv.normalize(src.astype('float'), None, 0.0, 1.0, cv.NORM_MINMAX)
     
     for cont in range(Num):
         #numpy.convolve(W, Y, mode='same')
-        F = np.exp(-Alpha_F) * F + V_F * numpy.convolve(W, Y, mode='same') + S
-        L = np.exp(-Alpha_L) * L + V_L * numpy.convolve(M, Y, mode='same')
+        F = np.exp(-Alpha_F) * F + V_F * signal.convolve2d(Y, W, mode='same') + S
+        L = np.exp(-Alpha_L) * L + V_L * signal.convolve2d(Y, M, mode='same')
         U = F * (1 + Beta * L)
         T = np.exp(-Alpha_T) * T + V_T * Y
         Y = (U>T).astype(np.float)
+        Y_AC = Y_AC + Y
+    
+    cv.imshow("Result", Y)
+    Y_AC = cv.normalize(Y_AC.astype('float'), None, 0.0, 1.0, cv.NORM_MINMAX)
+    cv.imshow("Result ac", Y_AC)
+        
+    cv.waitKey()
+    return 0
         
 if __name__ == "__main__":
     main(sys.argv[1:])
